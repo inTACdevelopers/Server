5144a4a8e9aaf1c3f3b7d345d8c91d82048c1f4d
Encapsulateed <69109441+Encapsulateed@users.noreply.github.com>
2022-09-11 17:50:18 +0300

    Make readme

---
+++ database/dump.sql
@@ -0,0 +1,166 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.7 (Ubuntu 13.7-1.pgdg20.04+1)
+-- Dumped by pg_dump version 13.7 (Ubuntu 13.7-1.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: posts; Type: TABLE; Schema: public; Owner: intac_net_ru_usr
+--
+
+CREATE TABLE public.posts (
+    id integer NOT NULL,
+    title character varying(255) NOT NULL,
+    description character varying(200) NOT NULL,
+    seller_contact character varying(255) NOT NULL,
+    from_user integer NOT NULL,
+    file_path character varying(512) NOT NULL,
+    creation_time character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.posts OWNER TO intac_net_ru_usr;
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE; Schema: public; Owner: intac_net_ru_usr
+--
+
+CREATE SEQUENCE public.posts_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.posts_id_seq OWNER TO intac_net_ru_usr;
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: intac_net_ru_usr
+--
+
+ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: intac_net_ru_usr
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    login character varying(255) NOT NULL,
+    password character varying(255) NOT NULL,
+    surname character varying(255) NOT NULL,
+    name character varying(255) NOT NULL,
+    company character varying(255),
+    birth_date character varying(255)
+);
+
+
+ALTER TABLE public.users OWNER TO intac_net_ru_usr;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: intac_net_ru_usr
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO intac_net_ru_usr;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: intac_net_ru_usr
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: posts id; Type: DEFAULT; Schema: public; Owner: intac_net_ru_usr
+--
+
+ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: intac_net_ru_usr
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: posts file_path; Type: CONSTRAINT; Schema: public; Owner: intac_net_ru_usr
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT file_path UNIQUE (file_path);
+
+
+--
+-- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: intac_net_ru_usr
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_login_key; Type: CONSTRAINT; Schema: public; Owner: intac_net_ru_usr
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_login_key UNIQUE (login);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: intac_net_ru_usr
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: posts user; Type: FK CONSTRAINT; Schema: public; Owner: intac_net_ru_usr
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT "user" FOREIGN KEY (from_user) REFERENCES public.users(id);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO intac_net_ru_grp;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- readme.md
+++ readme.md
@@ -1,2 +1,86 @@
-Server based on gRPC technology, it's allowed us to use microservices in our application
-Powered on python
+##gRPC server
+___
+
+>##Инструкция по использованию
+
+####Клонирование репозитория
+`-- git clone https://github.com/inTACdevelopers/Server.git`
+
+>###Создание базы данных
+> В своём проекте мы применили базу данных postgres.
+> Внутри папки database можно найти файл dump.sql, его нужно импортировать в базу данных
+
+>###Настройка окружения
+> Если среда разработки не предполагает автоматическое создание виртуального окружения, его необходимо создать
+>>Linux
+>> 
+>>`python -m venv venv`
+>>
+>>`source venv/bin/activate`
+>>
+>>Windows
+>>
+>>`python -m venv venv`
+>>
+>>`venv\Scripts\activate.bat`
+>
+> После включения виртуального окружения необходимо скачать пакеты python для работы с сервером
+>+ `pip install certifi`
+>+ `pip install charset-normalizer`
+>+ `pip install grpcio`
+>+ `pip install grpcio-tools`
+> + `pip install protobuf`
+> + `pip install psycopg2`
+> + `pip install requests`
+> + `pip install setuptools`
+
+
+
+>###Работа с файлами
+> В директории проекта `Server\database` необходимо создать директорию photos.
+> 
+> Также на уровне проекта необходимо создать файл conf.py
+> ```python
+> import os
+> 
+> HOST = "" #Ip адрес, на котором запущенн сервер postgreSQL
+> PORT = 0000 #Порт, который слушает база данных (обычно 5432)
+> USER = "" # Пользователь базы данных
+> PASSWORD = "" #Пароль пользователя базы данных
+> DB_NAME = "" #Имя базы данных
+> 
+> SERVER_IP = "" #Ip адрес удалённого сервера
+> SERVER_PORT = 0000 #Порт, на котором запущен сервер 
+> 
+> # Данные для запуска сервера локально
+> DEBAG_SERVER_IP = "localhost"
+> DEBAG_SERVER_PORT = 8080
+>
+> #Константа состояния:
+> #True - сервер запущен локально
+> #False - сервер запущен удалённо 
+> DEBAG = True
+>
+> #Путь до папки photos, созданной в прошлом шаге
+> DEBAG_PHOTO_DIR = os.getcwd() + "/database/photos/"
+> # Путь до папки photos, если сервер запускается НЕ локально
+> PHOTO_DIR = ""
+> ``` 
+> Этот файл определяет необходимые для работы сервера константы.
+> 
+
+Теперь сервер готов к запуску :)
+
+`python3 main.py`
+
+
+
+
+
+
+
+
+
+
+
+
